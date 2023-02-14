b048fd0befb0998fb7e5260e238e2cf34ab0052d
kkumar00 <124732805+kkumar00@users.noreply.github.com>
2023-02-13 22:15:55 -0500

    Add files via upload

---
+++ bday.py
@@ -0,0 +1,4 @@
+def bday (user):
+    print("Happy Birthday to you,\nHappy Birthday to you,\nHappy Birthday,dear",user,"\nHappy Birthday to you!")
+name = input('Enter you name:\n>>')
+print(bday(name))
